af39b101403dd7a1fe56260a49ede1e7ec403fda
MirkoCecchi <mirkocecchi1@gmail.com>
2026-01-08 02:58:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const cards =
+document.querySelectorAll ('.card');
+
+const observer = new
+IntersectionObserver(entries => {
+    entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+           setTimeout(() => {
+            entry.target.classList.add('show');
+           }, index * 300);
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+cards.forEach(card =>
+    observer.observe(card));
